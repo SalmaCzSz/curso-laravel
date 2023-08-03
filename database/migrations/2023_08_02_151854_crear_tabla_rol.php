c6cf350ccae6818cf23435870ae4dfb00d870097
Salma Cruz Sánchez <salma.cruz@fygdevelopers.com>
2023-08-03 10:00:07 -0600

    03/08/2023 | Se corrige atributo nombre

--- database/migrations/2023_08_02_151854_crear_tabla_rol.php
+++ database/migrations/2023_08_02_151854_crear_tabla_rol.php
@@ -15,7 +15,7 @@ class CrearTablaRol extends Migration
     {
         Schema::create('rol', function (Blueprint $table) {
             $table->bigIncrements('id')->autoIncrement() ;
-            $table->string('nombre', 50);
+            $table->string('nombre', 50)->unique();
             $table->timestamps();
             $table->charset = 'utf8mb4';
             $table->collation = 'utf8mb4_spanish_ci';
